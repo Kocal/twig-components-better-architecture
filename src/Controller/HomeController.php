edc2ad85adf8d8b9aa20a1bd06777fd8f71d98ba
Hugo Alliaume <hugo@alliau.me>
2024-07-28 18:03:46 +0200

    feat: create HomeController and change default styles

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+}

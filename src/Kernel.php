891b2d1e993064dc5f049dba49be316f0ef158ca
Hugo Alliaume <hugo@alliau.me>
2024-07-29 08:36:40 +0200

    imp(config): create ComponentExtension

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,10 +2,17 @@
 
 namespace App;
 
+use App\DependencyInjection\ComponentExtension;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    protected function build(ContainerBuilder $container): void
+    {
+        $container->registerExtension(new ComponentExtension('%kernel.project_dir%/component'));
+    }
 }

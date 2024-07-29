ee6c3effb3acc23bdc04840d8d878794cfd58205
Hugo Alliaume <hugo@alliau.me>
2024-07-29 08:23:15 +0200

    imp(Alert): lazy-load CSS

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,6 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+// import('../component/Alert/Alert.css');
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
--- component/Alert/alert_controller.js
+++ component/Alert/alert_controller.js
@@ -1,5 +1,5 @@
 import {Controller} from "@hotwired/stimulus";
-import './Alert.css';
+import('./Alert.css');
 
 export default class extends Controller {
     connect() {

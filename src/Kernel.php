454bbe75068d1874a77ebc1d9f79cb65d0cf65f7
Hugo Alliaume <hugo@alliau.me>
2024-07-29 09:05:43 +0200

    imp(config): create unique yaml config

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,17 +2,10 @@
 
 namespace App;
 
-use App\DependencyInjection\ComponentExtension;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-
-    protected function build(ContainerBuilder $container): void
-    {
-        $container->registerExtension(new ComponentExtension('%kernel.project_dir%/component'));
-    }
 }
